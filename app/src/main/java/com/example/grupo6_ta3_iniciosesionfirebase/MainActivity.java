a94604cb9703ec075f3ef4e1c24190fe7114b963
ppereira123 <ppereira@espol.edu.ec>
2020-11-22 15:05:42 -0500

    Creación de la aplicación

---
+++ app/src/main/java/com/example/grupo6_ta3_iniciosesionfirebase/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.grupo6_ta3_iniciosesionfirebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
