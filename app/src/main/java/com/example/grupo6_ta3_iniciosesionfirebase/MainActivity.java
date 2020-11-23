30dd9a0de691e757fb99f8a4b2effa21004f9a64
renatojaraor <57976123+renatojaraor@users.noreply.github.com>
2020-11-23 16:23:49 -0500

    Pasos 3 y 4 realizados

--- app/src/main/java/com/example/grupo6_ta3_iniciosesionfirebase/MainActivity.java
+++ app/src/main/java/com/example/grupo6_ta3_iniciosesionfirebase/MainActivity.java
@@ -19,6 +19,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
     //Varibales públicas
     static final int GOOGLE_SIGN_IN = 123;
@@ -36,12 +38,28 @@ public class MainActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra("msg");
+        if(msg != null){
+            if(msg.equals("cerrarSesion")){
+                cerrarSesion();
+            }
+        }
     }
     public void iniciarSesion(View view) {
+        System.out.println("Presionado Iniciar Sesion");
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, GOOGLE_SIGN_IN);
     }
+
+    private void cerrarSesion() {
+        mGoogleSignInClient.signOut().addOnCompleteListener(this,
+                task -> updateUI(null));
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        System.out.println("corriendo onActivity Result");
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == GOOGLE_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
@@ -55,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        System.out.println("Corriendo firebase Auth");
         Log.d("TAG", "firebaseAuthWithGoogle:" + acct.getId());
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
@@ -70,11 +89,16 @@ public class MainActivity extends AppCompatActivity {
     }
     private void updateUI(FirebaseUser user) {
         if (user != null) {
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            String photo = String.valueOf(user.getPhotoUrl());
-            System.out.println("nombre");
-            System.out.println(name);
+            HashMap<String, String> info_user = new HashMap<String, String>();
+            info_user.put("user_name", user.getDisplayName());
+            info_user.put("user_email", user.getEmail());
+            info_user.put("user_photo", String.valueOf(user.getPhotoUrl()));
+            info_user.put("user_id", user.getUid());
+
+            finish();
+            Intent intent = new Intent(this, PerfilUsuario.class);
+            intent.putExtra("info_user", info_user);
+            startActivity(intent);
         } else {
             System.out.println("sin registrarse");
         }
